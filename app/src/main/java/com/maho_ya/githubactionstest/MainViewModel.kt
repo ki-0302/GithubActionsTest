c0b0a899556f4454ee6a3ee039051aa351a7d579
mahoya <support@maho-ya.com>
2021-11-01 12:36:29 +0900

    test: テスト用データを追加

---
+++ app/src/main/java/com/maho_ya/githubactionstest/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.maho_ya.githubactionstest
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(
+    val context: Context
+): ViewModel() {
+
+    fun test() {
+        context
+    }
+}
